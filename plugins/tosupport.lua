777cfb9fc7dcc120fdc504f87a4d8771e540f877
2368t48rg4bqrf2t8trfg23rg7tbwsrf <m.h.1378.mh@gmail.com>
2016-02-24 08:12:01 -0800

    Update tosupport.lua

--- plugins/tosupport.lua
+++ plugins/tosupport.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return " The Support Invition Link : \n https://telegram.me/joinchat/CjspPQYo2phi5nnwvGFJXA"
+  return " The Support Invition Link : \n Coming soon:)"
   end
 return {
   description = "shows support link", 
