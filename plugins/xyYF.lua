bf2ef819379353967ca3e59a320593cc51fdfcc4
2368t48rg4bqrf2t8trfg23rg7tbwsrf <m.h.1378.mh@gmail.com>
2016-02-24 09:41:06 -0800

    B L A C K

---
+++ plugins/xyYF.lua
@@ -0,0 +1,43 @@
+--shared by @BlackCH
+do
+
+local function run(msg, matches)
+
+local bot_id = 171649341
+
+local fbotmain = 126388065
+
+
+    if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(72006705) and not is_sudo(msg) then
+
+--shared by @BlackCH
+
+  chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
+
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
+
+    end
+
+end
+
+
+
+return {
+
+  patterns = {
+
+    "^[$](bbye)$",
+
+    "^!!tgservice (.+)$",
+
+  },
+
+  run = run
+
+}
+
+end
+
+--shared by @BlackCH
