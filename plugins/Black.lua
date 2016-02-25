817de2ccb4de09edd9a3f1e35d568500fdebea19
2368t48rg4bqrf2t8trfg23rg7tbwsrf <m.h.1378.mh@gmail.com>
2016-02-25 14:24:55 +0330

    B L A C K

---
+++ plugins/Black.lua
@@ -0,0 +1,39 @@
+do
+
+
+function run(msg, matches)
+
+  return "🌐 B L A C K 🌐
+-------------------------
+A new bot for manage your groups.
+--------------------------------------------
+Our team:
+
+@Black_CH [channel]
+-----------------------------------
+@MehdiHS [developer]
+-----------------------------------
+@YflQw [manager]"
+
+end
+
+return {
+
+  description = "for show bor sudoers & ...", 
+
+  usage = "!join [invite link]",
+
+  patterns = {
+
+    "^/[Bb][Ll][Aa][Cc][Kk]$",
+
+    "
+
+
+  },
+
+  run = run
+
+}
+
+end
